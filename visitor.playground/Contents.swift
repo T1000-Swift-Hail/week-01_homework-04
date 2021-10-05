37c6d9ffb7a556dfef356ae6b82f1a9e353d6a87
ahmad1aa <Ahmed.Atallah.has.s@tuwaiq.edu.sa>
2021-10-06 00:05:00 +0300

    hm4 done

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let Myfriend = "ahmed"
+let temperature = 42
+
+if (temperature > 44 ){
+    print("ahmed it s hot . Don't go outside")
+} else if (temperature < 7 ){
+    print("ahmed it is freezing . Don't go outside")
+} else {
+    
+print("ahmed it is ok . You can go outside")
+}
