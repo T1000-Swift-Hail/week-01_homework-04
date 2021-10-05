2a8fe01dcd07094df3515116e34e5d0413e9af66
Anas Hamad <anashamad@MacBook-Air-Anas.local>
2021-10-05 17:04:57 +0300

    HW is done

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let Friend = "Ahmed"
+ var teperature = 40
+if (teperature > 44)||(teperature < 7) {
+    print("\(Friend)  .don't go outside")
+
+//} else if (teperature < 7){
+//    print("\(Friend) it is freezing .don't go outside")
+
+} else {
+    print("\(Friend) it is ok . you can go out side ")
+}
+
+
