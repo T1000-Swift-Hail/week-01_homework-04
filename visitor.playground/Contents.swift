608766bf237297da8e5204f81c58a1d024291f9a
user <user@MacBook-Air-.local>
2021-10-05 17:04:45 +0300

    done homework

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let friendname = "Ahmad"
+let temperature = 42
+
+if (temperature > 44) {
+    print("Ahmad it is hot . Don't go outside.")
+    
+}
+else if (temperature < 7) {
+    print("Ahmad it is freezing . Don't go outside.")
+    
+}
+else {
+    print("Ahmad  it is ok . You can go outside.")
+    
+}
