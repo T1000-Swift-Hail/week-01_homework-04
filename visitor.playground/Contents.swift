77ed0303fb839ae3429cb88fa1cf0365ca682899
MACBOOK <macbook@Az-MacBook-Pro.local>
2021-10-05 22:04:59 +0300

    the HW i'ts Done

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let yourfriend = "Ahmed"
+let temperatur: Int = 42
+
+if (temperatur > 44 ) {
+    
+    print("Ahmed it is hot. Don't go outside")
+    
+}
+
+else if (temperatur < 7 ) {
+    
+    print("Ahmed it is ftreezing . don't go outside")
+    
+} else {
+    
+    print("it is ok . you can go out side")
+} 
