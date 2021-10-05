139b455e3e23aed1252301b7fb7be5ca03b62e6e
Majed Alshammari <admin@Majeds-MacBook-Air.local>
2021-10-05 22:05:23 +0300

    w1-homework4 done

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let Myfriend: String = "Ahmed"
+let temprature: Int = 42
+
+if (temprature > 44) {
+    print("Ahmed it is hot . Don't go outside.")
+}
+else if (temprature < 7) {
+            print("Ahmed it is freezing . Don't go outside.")
+}
+else {
+    print("Ahmed it is ok . You can go outside.")
+}
