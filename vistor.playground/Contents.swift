2f39c7664833f3e478390a957bd16e9385de89cc
Mhabdu11 <Mohammed.Alhethely.has.s@tuwaiq.edu.sa>
2021-10-06 01:03:27 +0300

    done homework4

---
+++ vistor.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let myfriend = "ahmed"
+let temperature = 42
+
+if (temperature > 44 ){
+    print("ahmed it s hot . don,t go outside")
+} else if (temperature < 7 ){
+        print("ahmed it is freezing . don't go outside")
+} else {
+        
+    print("ahmed it is ok . you can go outside")
+}
