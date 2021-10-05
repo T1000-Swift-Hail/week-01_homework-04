52b43a7fb960d9e9fbf02b7d075221368ff12b63
mac <mac@MacBook-Pro-alkhas-b-mac.local>
2021-10-05 21:04:34 +0300

    I did my duty

---
+++ visiting .playground/Contents.swift
@@ -0,0 +1,19 @@
+
+var temperature = 35
+let myfrinedName : String = "Akram"
+
+if temperature > 42
+{ print("  \(myfrinedName) it is hot . Don't go outside.")}
+
+
+else if temperature > 0 && temperature < 7
+{print(" \(myfrinedName) it is freezing . Don't go outside.")}
+
+
+else
+{print("\(myfrinedName) it is ok . You can go outside.")}
+
+
+
+
+
