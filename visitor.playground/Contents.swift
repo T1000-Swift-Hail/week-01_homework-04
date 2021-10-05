cfe044597886f11cfd7027d34bfc0da54f5bad5e
hesah <hesah.raja.has.s@tuwaiq.edu.sa>
2021-10-05 23:02:51 +0300

    done4

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+let name = "Ahmed"
+
+let temperature = 42
+
+switch temperature {
+case 44:
+    
+    print ("my friend \(name)> it is hot . Don't go outside")
+    
+case 7:
+    print("my friend \(name)> it is freezing.don't go outside")
+    
+    
+    
+ 
+    
+default:
+    print ("my friend \(name) > it is ok. you can go outside")
+    
+}
