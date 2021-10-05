e375ebbd1e75a8366ee1ee8ea0dce3e6282400a9
hesah <hesah.raja.has.s@tuwaiq.edu.sa>
2021-10-06 00:13:27 +0300

    done4

--- visitor.playground/Contents.swift
+++ visitor.playground/Contents.swift
@@ -3,19 +3,18 @@ let name = "Ahmed"
 
 let temperature = 42
 
-switch temperature {
-case 44:
+if (temperature > 44 ) || (temperature == 44){
     
     print ("my friend \(name)> it is hot . Don't go outside")
     
-case 7:
+}
+
+    else if (temperature < 7){
     print("my friend \(name)> it is freezing.don't go outside")
     
-    
-    
- 
-    
-default:
-    print ("my friend \(name) > it is ok. you can go outside")
-    
 }
+    
+   
+print ("my friend \(name) > it is ok. you can go outside")
+    
+
