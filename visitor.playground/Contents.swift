49ba995f7313cce0e3fd43954aac51fc0b1d0604
Huda N S <hnsa5@MacBook-Air-Huda.local>
2021-10-05 21:06:32 +0300

    I completed my homework

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let myfrinednaem : String = "Ahmad"
+let temperature : Int = 42
+
+
+if (temperature == 44 || temperature > 44 ) {
+    print (" \(myfrinednaem) it is hot . Don't go outside.")
+}
+
+else if (temperature < 7 ) {
+    print (" \(myfrinednaem) it is freezing . Don't go outside")
+}
+else {
+    print (" \(myfrinednaem) it is ok . You can go outside.")
+}
