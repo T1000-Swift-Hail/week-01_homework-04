b2461e6b2c0ebaf4e4213c99c2d6f715a8a25dc5
dalal92 <dalal@MacBook-Pro.local>
2021-10-05 16:41:04 +0300

    first solution

---
+++ vositor.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+let friend = "Ahmed"
+var Temp = 44
+
+if Temp > 7 && Temp < 42 {
+    
+    print("\(friend) it is ok . You can go outside.")
+}
+else if Temp <= 7{
+    
+    print("\(friend) it is freezing . Don't go outside.")
+}
+
+else {
+    print("\(friend)  it is hot . Don't go outside.")
+
+}
+
+
+ 
+
+
+
