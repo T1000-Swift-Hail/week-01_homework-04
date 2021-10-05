6c6eb8d08c90ae9b9c0c71bdd42b26936624ab1f
Hind Alharbi <Hind.Alharbi.has.s@tuwaiq.edu.sa>
2021-10-05 23:43:16 +0300

    HW 4 is completed

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let myFriendName = "Ahmed"
+let temperature = 42
+
+if (temperature > 44){
+    
+    print(" \(myFriendName) it is hot . Don't go outside.")
+}
+    else if (temperature < 7){
+     print(" \(myFriendName) it is freezing . Don't go outside.")
+        
+    }
+    
+else {
+    print(" \(myFriendName) it is ok . You can go outside.")
+}
+
