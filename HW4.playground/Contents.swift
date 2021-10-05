f73ab6d4b0b09893a5d72d4ec9a3d14c1acf0955
Njoud Alrshidi <njoudalrshidi@MacBook-Pro-Njoud.local>
2021-10-06 00:06:08 +0300

    HW4 IS DONE

---
+++ HW4.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+
+let  myFriend = "Ahmed"
+let  temperature = 42
+
+
+if (temperature > 44) || (temperature == 42){
+    
+  print("\(myFriend) it is hot . Don't go outside.")
+}
+else if (temperature < 7){
+    print("\(myFriend) it is freezing . Don't go outside")
+    
+}
+else {
+    print("\(myFriend) it is ok . You can go outside.")
+    
+}
