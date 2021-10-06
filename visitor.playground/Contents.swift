3c655e04949e03fc691b29ab0bbcf23f76b677a6
hanaalmutlak <Hana.Khamis.has.s@tuwaiq.edu.sa>
2021-10-06 08:47:47 +0300

    solve homoork

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+
+let name = "Ahmed"
+let temperatur : Int = 44
+if (temperatur > 44) && (temperatur==44 ){
+    print("Ahmed hot thedont go outside/(temperatur)")
+    
+}
+    else if (temperatur<7){
+        print (" this to cold dont go outside")
+    }else {
+        print ("/(myname)the weather is ok you can go outside")
+        
+    }
+
