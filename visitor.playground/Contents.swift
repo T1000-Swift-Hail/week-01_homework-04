acd9f6451b6f3a8d6c857635127eced4ad0124eb
AsmaRashed1 <Asma.Rashed.has.s@tuwaiq.edu.sa>
2021-10-06 08:49:11 +0300

    Done

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+
+let friend : String = "Ahmad"
+let temperature : Int = 42
+if (temperature > 44) && (temperature == 1 ){
+    print (" Ahmad it is hot ; Don't go outside.")
+}
+else if (temperature < 7) {
+    print ( " Ahmad it is freezing . Don't go outside.")
+    
+}
+else {
+    
+    print ( " Ahmad it is ok . You can go outside."
+)
+}
+    
+
+
+
