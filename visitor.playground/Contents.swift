c17d2210ba8ac469f96b20d49a213c4ebdb99d49
Hind Alharbi <Hind.Alharbi.has.s@tuwaiq.edu.sa>
2021-10-06 07:40:31 +0300

    Bouns

--- visitor.playground/Contents.swift
+++ visitor.playground/Contents.swift
@@ -3,15 +3,12 @@ import UIKit
 var greeting = "Hello, playground"
 let myFriendName = "Ahmed"
 let temperature = 42
-
-if (temperature > 44){
+// Bouns
+if (temperature > 44) && (temperature < 7) {
     
     print(" \(myFriendName) it is hot . Don't go outside.")
 }
-    else if (temperature < 7){
-     print(" \(myFriendName) it is freezing . Don't go outside.")
-        
-    }
+
     
 else {
     print(" \(myFriendName) it is ok . You can go outside.")
