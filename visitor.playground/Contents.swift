3280d062d3b961d7a8dff8ff9712d411a05863ef
noyer altamimi <noyer.faisal.has.s@tuwaiq.edu.sa>
2021-10-06 08:06:32 +0300

    home work 4

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let myFriend = "Ahmed"
+let temperature = 42
+
+if (temperature > 44)  {
+       print( "myFriend, it is hot . Dont go outside.")
+       } else if
+    (temperature < 7) {
+print("myFriend, it is freezing Dont go outside")
+} else {
+print("myFriend it is ok . you can go outside")
+}
+(temperature > 44) && (temperature < 7)
+(temperature > 44) || ( temperature < 7)
