d78cba88d3823a3fba68d2693af74f630825d59f
Aryaf AlQabali <Aryaf.Aqeel.has.s@tuwaiq.edu.sa>
2021-10-06 05:02:54 +0300

    Done HW4

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let myFriend = "Ahmed"
+let temperature = 42
+
+
+if (temperature > 44) && (temperature == 1){
+   
+   print("\(myFriend) it is hot . Don't go outside.")
+}
+else if (temperature < 7){
+   
+   print("\(myFriend) it is freezing .  Don't go outside")
+   
+}
+else {
+   print("\(myFriend) it is ok . You can go outside.")
+}
