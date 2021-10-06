90d0d3744d44a78ce25016719b1fa0eb00191e5a
Seham الشطنان <sehamalshtnan@Sehams-MacBook-Air.local>
2021-10-06 08:38:23 +0300

    Done.

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+let myname = "Ahmed"
+let temperatur : Int = 44
+
+if (temperatur>44) && (temperatur==44 ){
+print ("Ahmed hot thedont go outside\(myname)")
+
+}  else if (temperatur<7){
+            print ("this to cold dont go outside")
+
+}else {
+    print("\(myname) the weather is ok you can go outside")
+
+}
+
