98d48b2e0fcb75b0811a8f9f8f636268ef807ac4
Munira abdullah <muniraabdullah@MacBook-alkhas-b-Munira.local>
2021-10-06 08:59:22 +0300

    finsh

---
+++ visito.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let myfrind = "Ahmad"
+let temperature = 42
+if ( temperature < 44 ) {
+    print("Ahmad it is not . Don't go outside.")
+} else if ( temperature > 7 ) {
+print("Ahmad it is freezing . Don't go outside.")
+} else {
+    print("Ahmad it is ok . you can go outside.")
+}
