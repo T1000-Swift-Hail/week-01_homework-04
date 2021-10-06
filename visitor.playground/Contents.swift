e07b321baab75dbd0213bdf23031ef5eb96465b0
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-06 09:00:05 +0300

    Don

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+let myname = "Ahmed"
+let temperur : Int = 44
+
+if ( temperur>44) && (temperur==44){
+    print("Ahmed hot thedont go outside\(myname)")
+    
+}   else if (temperur<7){
+        print("this to cold dont go outside")
+    
+} else {
+    print("\(myname)the weather is ok you can go outside")
+    
+}
+
