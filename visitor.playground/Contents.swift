d8c53d5db835cdb0c021cb7f1504ee415d9e294e
موضي الحربي <modhialhrbi@MacBook-Air-mwdy.local>
2021-10-06 09:02:12 +0300

    I did my homework

---
+++ visitor.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let myfriendname  = "Ahmed"
+let temperature = 42
+if (temperature > 44){
+    print(" < myfriendname > it is hot . Don't go outside.")
+}
+else if (temperature < 7) {
+  print(" < myfriendname  > it is freezing . Don't go outside.")
+} else {
+    print(" < myfriendname > it is ok . You can go outside.")
+}
