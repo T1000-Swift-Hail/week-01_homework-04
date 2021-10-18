35c9953f23c6e2ae8bbb061f881d9c4519c243a2
monaaleid <92249905+monaaleid@users.noreply.github.com>
2021-10-18 04:51:13 +0300

    done

---
+++ Vistor.playground/Contents.swift
@@ -0,0 +1,15 @@
+
+let frind = "Ahmed"
+let temp1 = 42
+let temp2 = 7
+
+if temp1 > 42 {
+    print((frind), "it is hot . Don't go outside.")
+          }else if temp2 < 7
+{
+              print((frind), "it is freezing . Don't go outside.")
+          }else{
+              print((frind), "it is ok . You can go outside")
+              
+          }
+
